e14f44d013d273c83272acb62da03386fb9d5bbb
eliasdevsilva-commits <eliasdevsilva@gmail.com>
2025-10-18 11:39:51 -0300

    Handle empty file

--- ByCodersChallenge/ByCodersChallenge.Presentation.AspNetCoreApi/Controllers/FinancialTransactions/FinancialTransactionController.cs
+++ ByCodersChallenge/ByCodersChallenge.Presentation.AspNetCoreApi/Controllers/FinancialTransactions/FinancialTransactionController.cs
@@ -2,9 +2,11 @@
 using BasePoint.Core.Application.Dtos.Input;
 using BasePoint.Core.Application.UseCases;
 using BasePoint.Core.Presentation.AspNetCoreApi.Controllers;
+using BasePoint.Core.Shared;
 using ByCodersChallenge.Core.Application.Dtos.FinancialServices;
 using ByCodersChallenge.Core.Application.Dtos.FinancialTransactions;
 using ByCodersChallenge.Core.Application.UseCases.FinancialTransactions;
+using ByCodersChallenge.Core.Shared;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ByCodersChallenge.Presentation.AspNetCoreApi.Controllers.FinancialTransactions
@@ -27,8 +29,13 @@ namespace ByCodersChallenge.Presentation.AspNetCoreApi.Controllers.FinancialTran
 
         [HttpPost]
         [ProducesResponseType(typeof(ImportFinancialTransactionsOutput), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(ImportFinancialTransactionsOutput), StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> ImportFinancialTransactions(IFormFile file)
         {
+            if (file is null || file.Length == 0)
+                return OutputConverter(new UseCaseOutput<ImportFinancialTransactionsOutput>(
+                    new List<ErrorMessage>() { new(SharedConstants.ErrorMessages.FileIsEmpty) }));
+
             using var memoryStream = new MemoryStream();
             await file.CopyToAsync(memoryStream);
 
@@ -37,11 +44,15 @@ namespace ByCodersChallenge.Presentation.AspNetCoreApi.Controllers.FinancialTran
                 MemoryStream = memoryStream
             };
 
-            return OutputConverter(await _importFinancialTransactionsUseCase.ExecuteAsync(input));
+            // Decided not to pass IFormFile directly to the use case because its from AspNet lib, and the use case should be agnostic from technology 
+            var output = await _importFinancialTransactionsUseCase.ExecuteAsync(input);
+
+            return OutputConverter(output);
         }
 
         [HttpPost("get-by-filter")]
         [ProducesResponseType(typeof(IEnumerable<FinancialTransactionListItemOutput>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(IEnumerable<FinancialTransactionListItemOutput>), StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> GetPaginatedFinancialTransaction(GetPaginatedResultsInput input)
         {
             return OutputConverter(await _getPaginatedFinancialTransactionUseCase.ExecuteAsync(input));
